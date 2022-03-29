33a9cc2136fe4fc84f84d9add74c90ed67debb5b
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2022-03-29 15:43:06 +0200

    Added error redirect

--- pages/error.js
+++ pages/error.js
@@ -1,4 +1,19 @@
+let seconds = 5;
+
 function error(){
     $("#content").empty();
     $("#content").load("error.html");
+    setInterval(UpdateText, 1000);
+    $("timer").text(seconds.toString());
 }
+function UpdateText(){
+    console.log("hallo")
+    if(seconds > 0){
+        seconds -= 1;
+        $("#timer").text("Redirecting in " + seconds + "s");
+    }
+    else{
+        window.location.href = "../index.html"
+    }
+}
+
